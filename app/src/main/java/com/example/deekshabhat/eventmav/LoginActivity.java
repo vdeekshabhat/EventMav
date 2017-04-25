bc87c7ea48e773d8fa509cdb627d259a946a4f7c
Deeksha Bhat <deekshabhat@Deekshas-MacBook-Pro.local>
2017-04-25 18:57:11 -0500

    setup

---
+++ app/src/main/java/com/example/deekshabhat/eventmav/LoginActivity.java
@@ -0,0 +1,137 @@
+package com.example.deekshabhat.eventmav;
+
+import android.app.ProgressDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.LoginFilter;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+
+
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText mUsername;
+    private EditText mPassword;
+    private Button mLogin;
+    private Button mRegister;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private String TAG = "login";
+
+//    public void initRegisterButton(){
+//        Button register=(Button)findViewById(R.id.mRegister);
+//        register.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent=new Intent(LoginActivity.this,Sign_up.class);
+//                startActivity(intent);
+//            }
+//        });
+//    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
+        mUsername = (EditText) findViewById(R.id.mUsername);
+        mPassword = (EditText) findViewById(R.id.mPassword);
+        mLogin = (Button) findViewById(R.id.mLogin);
+        mRegister = (Button) findViewById(R.id.mRegister);
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+
+                if (firebaseAuth.getCurrentUser() != null) {
+                    startActivity(new Intent(LoginActivity.this, HomeActivity.class));
+                }
+            }
+        };
+
+
+        mLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startSignIn();
+            }
+        });
+
+        mRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, Sign_up.class));
+            }
+        });
+
+
+    }
+    protected void onStart(){
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+
+
+    private void startSignIn() {
+        ProgressDialog progressDialog = new ProgressDialog(LoginActivity.this);
+
+        String email = mUsername.getText().toString();
+        String password = mPassword.getText().toString();
+
+        progressDialog.setMessage("Logging in...");
+        progressDialog.show();
+        if (TextUtils.isEmpty(email) || TextUtils.isEmpty(password)) {
+            Toast.makeText(LoginActivity.this, "Fields are empty", Toast.LENGTH_LONG).show();
+        } else {
+
+            mAuth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (!task.isSuccessful()) {
+                                Toast.makeText(LoginActivity.this, "Sign In Problem", Toast.LENGTH_LONG).show();
+                                Log.d("login", "NO");
+                            }
+                            else {
+                                Toast.makeText(LoginActivity.this, "Success", Toast.LENGTH_LONG).show();
+                                Log.d("login", "YES");
+                            }
+                        }
+                    });
+
+
+        }
+        progressDialog.dismiss();
+    }
+}
+
