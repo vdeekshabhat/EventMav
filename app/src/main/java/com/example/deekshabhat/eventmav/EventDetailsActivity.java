cba0fd47a050c02fa17a0236ebce4d1f6f527873
Ganeshbabu Thavasimuthu <ganeshbabu.thavasimuthu@mavs.uta.edu>
2017-05-05 13:25:26 +0600

    Added new location permission check before accessing the EventPath-MapRoute

--- app/src/main/java/com/example/deekshabhat/eventmav/EventDetailsActivity.java
+++ app/src/main/java/com/example/deekshabhat/eventmav/EventDetailsActivity.java
@@ -3,9 +3,12 @@ package com.example.deekshabhat.eventmav;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -48,7 +51,7 @@ public class EventDetailsActivity extends AppCompatActivity
     private String userID;
     private int count;
     private String eventName;
-
+    private static final int MY_PERMISSIONS_REQUEST_LOCATION = 101;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -69,9 +72,13 @@ public class EventDetailsActivity extends AppCompatActivity
         budetailsNavigate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(EventDetailsActivity.this, EventPathActivity.class);
-                intent.putExtra("eventAddress", tvDetailsEventLocation.getText().toString());
-                startActivity(intent);
+                if (checkLocationPermission()) {
+                    if (ContextCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                        Intent intent = new Intent(EventDetailsActivity.this, EventPathActivity.class);
+                        intent.putExtra("eventAddress", tvDetailsEventLocation.getText().toString());
+                        startActivity(intent);
+                    }
+                }
             }
         });
 
@@ -130,6 +137,74 @@ public class EventDetailsActivity extends AppCompatActivity
 
 
 
+    }
+
+    public boolean checkLocationPermission() {
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, android.Manifest.permission.ACCESS_FINE_LOCATION)) {
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+                new AlertDialog.Builder(this)
+                        .setTitle("Location Permission")
+                        .setMessage("We need your permission to access the device current location")
+                        .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                //Prompt the user once explanation has been shown
+                                ActivityCompat.requestPermissions(EventDetailsActivity.this,
+                                        new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                                        MY_PERMISSIONS_REQUEST_LOCATION);
+                            }
+                        })
+                        .create()
+                        .show();
+
+            } else {
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(this,
+                        new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_LOCATION);
+            }
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // location-related task you need to do.
+                    if (ContextCompat.checkSelfPermission(this,
+                            android.Manifest.permission.ACCESS_FINE_LOCATION)
+                            == PackageManager.PERMISSION_GRANTED) {
+                        Toast.makeText(this, "Location permission granted", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(EventDetailsActivity.this, EventPathActivity.class);
+                        intent.putExtra("eventAddress", tvDetailsEventLocation.getText().toString());
+                        startActivity(intent);
+                    }
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    Toast.makeText(this, "You can't access show map without location permission", Toast.LENGTH_SHORT).show();
+
+                }
+                return;
+            }
+
+        }
     }
 
     private void shareEvent(String message) {
